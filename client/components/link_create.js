1b3da72a66f25c8e305a360f441dc1281f517bb4
RAZ <rbrtzavala@gmail.com>
2017-05-19 10:37:57 -0600

    Initialized state on component for error. Updated handleSubmit method to display an error message if url does not pass validation

--- client/components/link_create.js
+++ client/components/link_create.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 
 class LinkCreate extends Component {
+  // initialize component state inside a contructor
+  constructor(props) {
+    super(props);
+
+    this.state = { error: '' };
+  }
+
   handleSubmit(event) {
     // prevent browser default behavior
     event.preventDefault();
 
     // refs: reference => ref attribute on input tag
-    // console.log(this.refs.input.value);
+    // console.log(this.refs.link.value);
     // Call our Meteor Methiod
-    Meteor.call('links.insert', this.refs.input.value);
+    Meteor.call('links.insert', this.refs.link.value, (error) => {
+      if (error) {
+        this.setState({ error: 'Please enter a valid URL' });
+      } else {
+        this.setState({ error: '' });
+        this.refs.link.value = '';
+      }
+    });
   }
 
   render() {
@@ -16,8 +30,9 @@ class LinkCreate extends Component {
       <form onSubmit={this.handleSubmit.bind(this)}>
         <div className="form-group">
           <label>Link to Shorten</label>
-          <input ref="input" className="form-control" />
+          <input ref="link" className="form-control" />
         </div>
+        <div className="text-danger">{this.state.error}</div>
         <button className="btn btn-primary">Shorten!</button>
       </form>
     );
