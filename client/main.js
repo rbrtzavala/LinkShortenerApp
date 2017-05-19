52667bce005d59cbb28a0269b890bd0a15ed2705
RAZ <rbrtzavala@gmail.com>
2017-05-19 10:09:12 -0600

    Import Links collection so that client-side has access to meteor methods decalred

--- client/main.js
+++ client/main.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 
 import Header from './components/header';
 import LinkCreate from './components/link_create';
+// Import Links collection so client has access to any
+// Meteor.mthods() declared
+import { Link } from '../imports/collections/links';
 
 const App = () => {
   return (
