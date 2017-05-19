852b358252f38e1472f98d737d30e716d3fa8ffd
RAZ <rbrtzavala@gmail.com>
2017-05-19 10:09:31 -0600

    Import Links collection so that server-side has access to meteor methods decalred

--- server/main.js
+++ server/main.js
@@ -1,5 +1,9 @@
 import { Meteor } from 'meteor/meteor';
 
+// Import Links collection so server has access to any
+// Meteor.mthods() declared
+import { Links } from '../imports/collections/links';
+
 Meteor.startup(() => {
   // code to run on server at startup
 });
