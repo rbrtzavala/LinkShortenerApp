b2cea4f8bf2b68f77d3388aad80001afdf16cae7
RAZ <rbrtzavala@gmail.com>
2017-05-19 10:10:05 -0600

    Created meteor method links.insert

--- imports/collections/links.js
+++ imports/collections/links.js
@@ -1,5 +1,11 @@
 import { Mongo } from 'meteor/mongo';
 
+// Define a method that takes in an object
+Meteor.methods({
+  'links.insert': function(url) {
+    console.log('attempting to save', url);
+  }
+});
 
 // create new collection 'Links'
 export const Links = new Mongo.Collection('links');
