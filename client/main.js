850804e03e65e19c4c55c5caca3dbf5768d1bb54
RAZ <rbrtzavala@gmail.com>
2017-05-19 09:20:27 -0600

    Imported Header to App

--- client/main.js
+++ client/main.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Header from './components/header';
 const App = () => {
   return (
-    <h1>Hello World!</h1>
+    <div>
+      <Header />
+    </div>
   );
 };
 
