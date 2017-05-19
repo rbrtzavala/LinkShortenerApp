aa9d3ec367b55d96fd8ba5b2682300dcdca66d0c
RAZ <rbrtzavala@gmail.com>
2017-05-19 11:09:15 -0600

    Created meteor publication to links collection

--- server/main.js
+++ server/main.js
@@ -6,4 +6,8 @@ import { Links } from '../imports/collections/links';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  // Setup publication for access on client-side
+  Meteor.publish('links', function() {
+    return Links.find({});
+  });
 });
