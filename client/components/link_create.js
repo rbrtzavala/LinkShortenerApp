1a85b698ad88d92cb273e3562a545d30ddabdf6f
RAZ <rbrtzavala@gmail.com>
2017-05-19 09:43:45 -0600

    Create LinkCreate component rendering a form to page

---
+++ client/components/link_create.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class LinkCreate extends Component {
+  handleSubmit(event) {
+    // prevent browser default behavior
+    event.preventDefault();
+    
+    // refs: reference => ref attribute on input tag
+    console.log(this.refs.input.value);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit.bind(this)}>
+        <div className="form-group">
+          <label>Link to Shorten</label>
+          <input ref="input" className="form-control" />
+        </div>
+        <button className="btn btn-primary">Shorten!</button>
+      </form>
+    );
+  }
+}
+
+export default LinkCreate;
