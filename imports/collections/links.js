65aebe4f2775ec883d6c32388d32b0da957d4508
RAZ <rbrtzavala@gmail.com>
2017-05-19 10:19:38 -0600

    Imported validUrl and check and Match to handle our url validation in app

--- imports/collections/links.js
+++ imports/collections/links.js
@@ -1,9 +1,13 @@
 import { Mongo } from 'meteor/mongo';
+import validUrl from 'valid-url';
+import { check, Match } from 'meteor/check';
 
 // Define a method that takes in an object
 Meteor.methods({
   'links.insert': function(url) {
-    console.log('attempting to save', url);
+    // npm package that validates for us.
+    // validUrl.isUri(url);
+    check(url, Match.Where(url => validUrl.isUri(url)));
   }
 });
 
